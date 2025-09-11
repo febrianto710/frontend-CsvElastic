9e998c45723e38796e14d35d459e54c521260f83
Febrianto <ikxe496@gmail.com>
2025-09-11 11:13:19 +0700

    feat: update settings config

--- src/config/settings.js
+++ src/config/settings.js
@@ -2,5 +2,5 @@ export const BASE_API_URL = "http://localhost:5000";
 
 export const INDEX_TYPES = [
   { value: "employee", view: "Data Karyawan" },
-  { value: "test", view: "Test" },
+  { value: "web portal", view: "Web Portal" },
 ];
